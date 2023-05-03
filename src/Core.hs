4359c68bdfadf4862b675e6e9d1f2428bc304e95
Equwece <99554597+Equwece@users.noreply.github.com>
2023-05-03 20:51:31 +0300

    Change pomodoro state

--- src/Core.hs
+++ src/Core.hs
@@ -32,12 +32,31 @@ data PomodoroState = Work | Rest | LongRest deriving (Eq, Show)
 makeLenses ''AppState
 
 drawUI :: AppState -> [Widget ()]
-drawUI currentState = [str (show $ currentState ^. currentTimer) <=> str (show $ currentState ^. currentPomodoroState)]
+drawUI currentState =
+  [ str (renderCurrentTimer $ currentState ^. currentTimer)
+      <=> str (show $ currentState ^. currentPomodoroState)
+  ]
+
+renderCurrentTimer :: Int -> String
+renderCurrentTimer currentTime = minutes <> ":" <> seconds
+  where
+    minutes = show $ div currentTime 60
+    seconds = show $ mod currentTime 60
 
 handleEvent :: AppState -> BrickEvent n PomodoroEvent -> EventM n (Next AppState)
 handleEvent currentState (AppEvent Second) = do
-  continue $ decreasePomodoroTimer currentState
+  continue $ getNewState currentState
 handleEvent currentState be = resizeOrQuit currentState be
+
+getNewState :: AppState -> AppState
+getNewState currentState = case currentState ^. currentTimer of
+  0 -> decreasePomodoroTimer . decideNewPomodoroState $ currentState
+  _ -> decreasePomodoroTimer currentState
+
+decideNewPomodoroState :: AppState -> AppState
+decideNewPomodoroState currentState = case currentState ^. currentPomodoroState of
+  Work -> currentState & currentPomodoroState .~ Rest & currentTimer .~ (currentState ^. restTime)
+  _ -> currentState & currentPomodoroState .~ Work & currentTimer .~ (currentState ^. workTime)
 
 decreasePomodoroTimer :: AppState -> AppState
 decreasePomodoroTimer currentState = currentState & currentTimer -~ 1
